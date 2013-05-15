07c4fabb4cfbd9446bef9724fcc680eff3d06041
Sachin Honnudike <shonnudi@student.umass.edu>
2013-05-15 18:57:22 -0400

    ClockAideTime
    ClockAideTime

---
+++ ClockAideTime.py
@@ -0,0 +1,21 @@
+import string, time, datetime, re
+
+#Return the complete time and date in string format
+#Required to initialize the stepper motors
+def getDateTimeString():
+	currentTime = datetime.datetime.now()
+	#print(currentTime.strftime("%I%M"))
+	return currentTime.strftime("%H, %M, %S, %d, %m, %Y")
+
+#Returns the hour and minute in string format
+def getTimeString():
+    currentTime = datetime.datetime.now()
+    #print(currentTime.strftime("%I%M"))
+    return currentTime.strftime("%H, %M")
+
+def getTimeTuple():
+	currentTime = datetime.datetime.now()
+	hour = currentTime.strftime("%H")
+	minute = currentTime.strftime("%M")
+	timeTouple = hour, minute
+	return timeTouple
